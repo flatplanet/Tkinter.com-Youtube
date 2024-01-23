29b36d8552adc0d200dd586525c3c5f6c031744d
John Elder <johne4196@gmail.com>
2024-01-23 10:50:46 -0800

    oop popup boxes

--- class_popups.py
+++ class_popups.py
@@ -11,8 +11,8 @@ class App(Tk):
 		self.geometry('700x450')
 
 		# Create Widgets
-		self.my_label = Label(self, text="Enter Your Name:"
-			, font=("Helvetica", 24))
+		self.my_label = Label(self, text="Enter Your Name:", 
+			font=("Helvetica", 24))
 		self.my_label.pack(pady=20)
 
 		self.my_entry = Entry(self, width=30, font=("Helvetica", 24))
@@ -20,8 +20,8 @@ class App(Tk):
 
 		self.my_button = Button(self, text="Popup", command=self.popup)
 		self.my_button.pack(pady=20)
-
-	# Create Popup function
+	
+	# Create Popup Function
 	def popup(self):
 		#showinfo()
 		#showwarning()
@@ -33,7 +33,7 @@ class App(Tk):
 		if self.my_entry.get():
 			messagebox.showinfo("Hello", f"Hello {self.my_entry.get()}")
 		else:
-			messagebox.showerror("Error", "You Forgot To Enter Your Name!")
+			messagebox.showerror("Error", "You Forgot To Type In Your Name!! Try Again")
 
 # Define and instantiate our app
 app = App()
