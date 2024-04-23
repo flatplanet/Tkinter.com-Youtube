8f5156cacccc6f46c987617ac4b5665f2ff91aa7
John Elder <johne4196@gmail.copm>
2024-04-23 09:36:09 -0700

    status bar

--- status.py
+++ status.py
@@ -6,19 +6,21 @@ root.iconbitmap('images/tkinter.ico')
 root.geometry('600x400')
 
 global count
-count=1
+count = 1
 def next():
 	global count
 	my_status.config(text=f"{count} of 100  ")
-	count +=1
+	#increment the counter
+	count += 1
+	
 
-my_button = Button(root, text="Next >>", command=next, font=("Helvetica", 24))
+
+my_button = Button(root, text="Next >>", font=("Helvetica", 18), command=next)
 my_button.pack(pady=100)
 
-# Create our status bar
-my_status = Label(root, text="0 of 100  ", anchor=E, relief="sunken", bd=1) # raised, sunken, flat, ridge, groove
-my_status.pack(side="bottom", fill="x", ipady=2)
-
+# Create a status bar
+my_status = Label(root, text="0 of 100  ", anchor=E, relief="sunken", bd=1) # flat, sunken, raised, groove, ridge
+my_status.pack(fill="x", side="bottom", ipady=2)
 
 
 
